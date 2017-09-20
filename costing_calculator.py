3db32bdc78962e7995910e94609dc91007b11211
Risko Tiitso <31932570+risko90@users.noreply.github.com>
2017-09-20 20:42:02 +0300

    Calculator started
    The very beginning of costing calculator.

---
+++ costing_calculator.py
@@ -0,0 +1,12 @@
+#Let the user know that this costing has started and after hitting ENTER
+#he or she can either select current data or modify variables.
+
+print ('Costing calculator is now running.')
+print ('After pressing ENTER you can select current data as base or adjust these yourself.')
+print (' ')
+input ('Press ENTER to continue: ') 
+#Set up all needed variables with data and print them to user.
+laborcost = 1000
+averagehours = 156
+print ('Monthly labor cost =', laborcost)
+print ('Average monthly working hours =', averagehours)
