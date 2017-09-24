4636572367cbf98e28c6e4d9db4f08f6dcbb7e75
Risko Tiitso <31932570+risko90@users.noreply.github.com>
2017-09-24 18:34:48 +0300

    User selection if-elif-else
    Created function. User can change initial data or use the same and continue with bill of material.

--- costing_calculator.py
+++ costing_calculator.py
@@ -22,22 +22,46 @@ packagingcost = 0.01 #packaging cost per PC
 profitpercent = 1/10
 roomrent = 500 #roomrent including electricity, internet etc...
 machinery = 100
-print ('Operator cost per month =', operatorcost,'EUR')
-print ('Average monthly working hours =', averagehours)
-
-#After printing ask user if they want to change them and ask to change one by one, typing y = yes and n = no
-
-
 
 #Here you need to input common costing parameters. How much is work time per PC etc...
 assembling = 1 #One minute per PC
-packing = 0.01 #
-
-
+packing = 5/60 #Five seconds per PC
 compflagpole = 0.05 #price per PC
 compflag = 0.30 #price per meter length
 compyarn = 0.03 #price per meter length
 compbase = 0.04 #price per PC
+volume = 1800
+
+print ('Operator cost per month =', operatorcost,'EUR')
+print ('Average monthly working hours =', averagehours)
+print ('Scrap percentage is =', scraprate, '%')
+print ('Packaging cost per PC =', packagingcost, 'EUR')
+print ('Profit target =', profitpercent, 'EUR')
+print ('Room rent a month =', roomrent, 'EUR')
+print ('Machinery cost per month =', machinery, 'EUR')
+print (' ')
+
+#After printing ask user if they want to change them and ask to change one by one, typing y = yes and n = no
+print ('Now you can choose, if you want to change this data or continue.')
+
+def choose():
+    selection = input('Enter Y to change data and N to continue: ')
+    selection.upper()
+    if selection.upper() == 'Y': #Let the user change data.
+        operatorcost = input ('New operator cost per month is = ')
+        officesalary = input ('New office salary per month is = ')
+        averagehours = input ('New average working hours per month = ')
+        scraprate = input ('New packaging cost per PC = ')
+        profitpercent = input ('New profit before taxes percentage = ')
+        roomrent = input ('New room rent per month = ')
+        machinerycost = input ('New machinery cost per month = ')
+    elif selection.upper() == 'N': #Use the same data and continue.
+        print ('You did not want to change data.')
+    else:
+        print ('Incorrect input, please select out of the two options')
+        choose()    
+choose()
+
 print (' ')
 print (' ')
 print ('Bill of Material is as follows:')
@@ -46,6 +70,9 @@ print ('0.10m long flag, cost per meter length =', compflag,'EUR')
 print ('0.50m yarn, cost per meter length =', compyarn,'EUR')
 print ('One base for flag standing, cost per PC =', compbase,'EUR')
 print (' ')
+
+
+
 
 #Set variables for flag and yarn - quanitity needed per one finish product.
 flagquant = 0.1
