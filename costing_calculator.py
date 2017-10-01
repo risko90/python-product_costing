265e7e0d036c019c18877bc6da8dfa8a4d5ae858
Risko Tiitso <31932570+risko90@users.noreply.github.com>
2017-10-01 21:05:06 +0300

    Created costing summary table (tabulate)
    Created first table with selling price, materials cost, direct cost, overheads cost, EBT. Also changed print (' ') to /n in most cases. Still need to improve costing summary table and add other table with total amounts in EUR.

--- costing_calculator.py
+++ costing_calculator.py
@@ -1,18 +1,17 @@
 #Let the user know that this costing has started and after hitting ENTER
 #he or she can either select current data or modify variables.
+from tabulate import tabulate
 
 print ('Costing calculator is now running.')
 print ('After pressing ENTER you can select current data as base or adjust these yourself.')
-print (' ')
-input ('Press ENTER to continue: ') 
+input ('\nPress ENTER to continue: ') 
 
 #Describe user the product and production space.
-print ('We need to calculate table flag selling price for 900PCs per month.')
+print ('\nWe need to calculate table flag selling price for 900PCs per month.')
 print ('Flag has 4 different components in bill of material.')
 print ('Office and production is in the same small room.')
 print ('Office and production worker work full-time.')
-print (' ')
-print ('Here are current cost levels and figures')
+print ('\nHere are current cost levels and figures')
 #Set up all needed variables with data and print them to user.
 operatorcost = 1000
 officesalary = 1500
@@ -25,12 +24,13 @@ machinery = 100
 
 #Here you need to input common costing parameters. How much is work time per PC etc...
 assembling = 1 #One minute per PC
-packing = 5/60 #Five seconds per PC
+packing = 5 #Five seconds per PC
 compflagpole = 0.05 #price per PC
 compflag = 0.30 #price per meter length
 compyarn = 0.03 #price per meter length
 compbase = 0.04 #price per PC
-volume = 1800
+volume = 100000
+productiontime = (volume / (assembling + packing/60)/60)
 
 print ('Operator cost per month =', operatorcost,'EUR')
 print ('Average monthly working hours =', averagehours)
@@ -39,10 +39,9 @@ print ('Packaging cost per PC =', packagingcost, 'EUR')
 print ('Profit target =', profitpercent, 'EUR')
 print ('Room rent a month =', roomrent, 'EUR')
 print ('Machinery cost per month =', machinery, 'EUR')
-print (' ')
 
 #After printing ask user if they want to change them and ask to change one by one, typing y = yes and n = no
-print ('Now you can choose, if you want to change this data or continue.')
+print ('\nNow you can choose, if you want to change this data or continue.')
 
 def choose():
     selection = input('Enter Y to change data and N to continue: ')
@@ -62,23 +61,46 @@ def choose():
         choose()    
 choose()
 
-print (' ')
-print (' ')
-print ('Bill of Material is as follows:')
+print ('\nBill of Material is as follows:')
 print ('One flag pole, cost per PC =', compflagpole,'EUR')
 print ('0.10m long flag, cost per meter length =', compflag,'EUR')
 print ('0.50m yarn, cost per meter length =', compyarn,'EUR')
 print ('One base for flag standing, cost per PC =', compbase,'EUR')
-print (' ')
-
-
-
 
 #Set variables for flag and yarn - quanitity needed per one finish product.
 flagquant = 0.1
 yarnquant = 0.5
 BOMcost = compflagpole + (compflag * flagquant) + (compyarn * yarnquant) + compbase
+scrapcost = scraprate * BOMcost
+BOMcost = BOMcost + scrapcost
+directcost = ((operatorcost / averagehours) * productiontime) / volume
+machinerycost = ((machinery / averagehours) * productiontime) / volume
+overheadcost = (((officesalary + roomrent) / averagehours) * productiontime) / volume
+salesprice = (BOMcost + directcost + machinerycost + overheadcost) / (1 - profitpercent)
+EBT = salesprice - (BOMcost + directcost + machinerycost + overheadcost)
 
-#print random formulas as test
-print ('BOM cost per PC =', BOMcost,'EUR')
+print(' ')
+table = [['Sales price', round (salesprice, 3)],
+         ['Gross Margin %', round (((salesprice - BOMcost) / salesprice) * 100, 1)],
+         ['Materials', round (BOMcost, 3)],
+         ['Materials %', round ((BOMcost / salesprice) * 100, 1)],
+         ['Sales Margin %', round (((salesprice - BOMcost - directcost) / salesprice) * 100, 1)],
+         ['Direct work', round (directcost, 3)],
+         ['Direct work %', round ((directcost / salesprice) * 100, 1)],
+         ['Machinery', round (machinerycost, 3)],
+         ['Overheads', round (overheadcost, 3)],
+         ['Overheads %', round ((overheadcost / salesprice) * 100, 1)],
+         ['EBT', round (EBT, 3)],
+         ['EBT%', round ((EBT / salesprice) * 100, 1)]]
+headers = ["Yearly volume", volume]
+print (tabulate(table, headers, tablefmt = "grid"))
+
+
+
+
+
+
+
+
+
 
